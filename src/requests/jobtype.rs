d2de269cf6ae46801b50e2068c531ed70a8c824c
L8352 <L8352@student.jamk.fi>
2022-04-21 21:34:13 +0300

    Refactor Job enum to CientJob and ServerJob.

--- src/requests/jobtype.rs
+++ src/requests/jobtype.rs
@@ -1,31 +1,95 @@
-pub enum JobType {
-    DATA_PUSH_ACTION,
-    DATA_PUSH_RECEIVED,
-    DATA_REQUEST,
-    PLAYER_ENTER,
-    PLAYER_LEAVE,
-    PING,
+pub type JobType = (ServerJob, ClientJob);
+
+pub enum ClientJob {
+    NoClientAction,
+    DataPushRequest,
+    DataRequest,
+    PlayerEnterRequest,
+    PlayerLeaveRequest,
+    PingRequest,
 }
 
-pub fn transfer_job_type_to_byte(job_type: &JobType) -> u8 {
-    match job_type {
-        JobType::DATA_PUSH_ACTION => 1,
-        JobType::DATA_PUSH_RECEIVED => 2,
-        JobType::DATA_REQUEST => 3,
-        JobType::PLAYER_ENTER => 4,
-        JobType::PLAYER_LEAVE => 5,
-        JobType::PING => 6,
-    }
+pub enum ServerJob {
+    NoServerAction,
+    DataPush,
+    DataPushDoneResponse,
+    DataResponse,
+    PlayerCreatedResponse,
+    PlayerEnterPush,
+    PlayerLeaveResponse,
+    PlayerLeavePush,
+    PongResponse,
 }
 
-pub fn tranfer_job_type_from_number(job_type_number: &u8) -> Option<JobType> {
-    match job_type_number {
-        1 => Some(JobType::DATA_PUSH_ACTION),
-        2 => Some(JobType::DATA_PUSH_RECEIVED),
-        3 => Some(JobType::DATA_REQUEST),
-        4 => Some(JobType::PLAYER_ENTER),
-        5 => Some(JobType::PLAYER_LEAVE),
-        6 => Some(JobType::PING),
+pub fn get_job_bytes(job_type: &JobType) -> (u8, u8) {
+    (
+        match job_type.0 {
+            ServerJob::NoServerAction => 0,
+            ServerJob::DataPush => 1,
+            ServerJob::DataPushDoneResponse => 2,
+            ServerJob::DataResponse => 3,
+            ServerJob::PlayerCreatedResponse => 4,
+            ServerJob::PlayerEnterPush => 5,
+            ServerJob::PlayerLeaveResponse => 6,
+            ServerJob::PlayerLeavePush => 7,
+            ServerJob::PongResponse => 8,
+        },
+        match job_type.1 {
+            ClientJob::NoClientAction => 0,
+            ClientJob::DataPushRequest => 1,
+            ClientJob::DataRequest => 2,
+            ClientJob::PlayerEnterRequest => 3,
+            ClientJob::PlayerLeaveRequest => 4,
+            ClientJob::PingRequest => 5,
+        },
+    )
+}
+
+pub fn get_job_single_byte(job_type: &JobType) -> u8 {
+    let job_bytes = get_job_bytes(job_type);
+    println!("{}, {}", job_bytes.0, job_bytes.1);
+    create_job_type_byte(&job_bytes)
+}
+
+fn create_job_type_byte(server_client: &(u8, u8)) -> u8 {
+    let (server, client) = server_client;
+    println!("{:#b}", server);
+    println!("{:#b}", client);
+
+    let server_left_bits = server << 4;
+    println!("{:#b}", server_left_bits);
+    server_left_bits | client
+}
+
+pub fn get_job_type(server_client: &(u8, u8)) -> Option<JobType> {
+    let (server, client) = server_client;
+
+    let server_job = match server {
+        0 => Some(ServerJob::NoServerAction),
+        1 => Some(ServerJob::DataPush),
+        2 => Some(ServerJob::DataPushDoneResponse),
+        3 => Some(ServerJob::DataResponse),
+        4 => Some(ServerJob::PlayerCreatedResponse),
+        5 => Some(ServerJob::PlayerEnterPush),
+        6 => Some(ServerJob::PlayerLeaveResponse),
+        7 => Some(ServerJob::PlayerLeavePush),
+        8 => Some(ServerJob::PongResponse),
         __ => None,
+    };
+
+    let client_job = match client {
+        0 => Some(ClientJob::NoClientAction),
+        1 => Some(ClientJob::DataPushRequest),
+        2 => Some(ClientJob::DataRequest),
+        3 => Some(ClientJob::PlayerEnterRequest),
+        4 => Some(ClientJob::PlayerLeaveRequest),
+        5 => Some(ClientJob::PingRequest),
+        __ => None,
+    };
+
+    if server_job.is_none() || client_job.is_none() {
+        return None;
     }
+
+    Some((server_job.unwrap(), client_job.unwrap()))
 }
