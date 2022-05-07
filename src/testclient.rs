7cf721aa24c75898790f59fbb47d401ccd24bc2e
L8352 <L8352@student.jamk.fi>
2022-05-07 12:50:54 +0300

    Add dev testing functionality.

--- src/testclient.rs
+++ src/testclient.rs
@@ -1,6 +1,7 @@
+use crate::client;
 use crate::client::client::Client;
 use crate::client::RequestEvents;
-use crate::requests::jobtype::JobType;
+use crate::requests::jobtype::{ClientJob, ServerJob};
 use std::io::stdin;
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -17,19 +18,20 @@ impl RequestEvents for Client {
     }&mut self&mut self
 }
 */
-
-fn handle_data_cb(job_type: JobType, raw_data: &mut [u8]) {
+/*
+fn handle_data_cb(job_type: ClientJobType, raw_data: &mut [u8]) {
     match job_type {
-        JobType::DATA_PUSH_ACTION => {}
-        JobType::DATA_PUSH_RECEIVED => {}
-        JobType::DATA_REQUEST => {}
-        JobType::PLAYER_ENTER => {}
-        JobType::PLAYER_LEAVE => {}
-        JobType::PING => {
+        ClientJobType::DataPushRequest => {}
+        ClientJobType::DataRequest => {}
+        ClientJobType::Data => {}
+        ClientJobType::PLAYER_ENTER => {}
+        ClientJobType::PLAYER_LEAVE => {}
+        ClientJobType::PING => {
             println!("Pong")
         }
     }
 }
+*/
 
 struct Eventful {
     pub count: u128,
@@ -53,14 +55,26 @@ impl RequestEvents for Eventful {
     fn on_data_request(&mut self, _: std::vec::Vec<u8>) {
         todo!()
     }
-    fn on_player_enter(&mut self, _: u8, _: std::vec::Vec<u8>) {
-        todo!()
+    fn on_player_enter(&mut self, player: u8, raw_data: std::vec::Vec<u8>) {
+        println!("Entered player number {}", player);
     }
+    fn on_player_created(&mut self, player: u8, raw_data: std::vec::Vec<u8>) {
+        println!("Created player number {}", player);
+    }
+
     fn on_player_leave(&mut self, _: std::vec::Vec<u8>) {
         todo!()
     }
     fn on_error(&mut self) {
         println!("UDP ERROR!!!");
+    }
+    fn on_connection_state_change(&mut self, error_state: bool) {
+        if error_state {
+            println!("Connection is lost");
+        }
+        if !error_state {
+            println!("COnnection established");
+        }
     }
 }
 
@@ -68,13 +82,24 @@ pub fn testclient() {
     println!();
     println!("UDP Rusting client...");
 
+    let server_job = ServerJob::DataResponse;
+    let client_job = ClientJob::DataPushRequest;
+
+    let byte = crate::requests::jobtype::get_job_single_byte(&(server_job, client_job));
+    println!("{:#b}", byte);
+
+    let server = crate::protocol::datastructure::get_u8_from_bit_slice(byte, 0, 4);
+    let client = crate::protocol::datastructure::get_u8_from_bit_slice(byte, 4, 4);
+
+    println!("{},{}", server, client);
+
+    /*
     let first: u8 = 0b10101110;
     let second: u8 = 0b11110010;
     let third: u8 = 0b10001111;
 
     let bytes = vec![first, second, third];
 
-    /*
     let mut data_s = DataStructure::new(bytes);
 
     data_s.add_structure(
@@ -101,6 +126,7 @@ pub fn testclient() {
         "{}",
         data_s.get_u8_from_bit_slice("test2".to_string()).unwrap()
     );
+
     */
 
     println!("port: ");
@@ -109,9 +135,16 @@ pub fn testclient() {
     stdin().read_line(&mut port).expect("Shit happened...");
     local_ip.push_str(&port.trim());
 
-    let mut client = Client::new(&local_ip, "localhost:11111", 3, handle_data_cb);
+    let mut client = Client::new(3);
+
+    let result = client.connect(local_ip, "localhost:11111".to_string());
+    if result.is_err() {
+        println!("Connection failed");
+        return;
+    }
+
     let events = Arc::new(Mutex::new(Eventful { count: 0 }));
-    client.run(events);
+    client::run(&mut client, events);
 
     println!("Pause between data grams: ");
     let mut pause_time = String::new();
@@ -126,15 +159,36 @@ pub fn testclient() {
     let mut stuff = String::new();
     stdin().read_line(&mut stuff).expect("Shit happened...");
 
+    println!("Player name: ");
+    let mut name = String::new();
+    stdin().read_line(&mut name).expect("Shit");
+    let name_owned = name.trim().to_owned();
+    let mut name_bytes = name_owned.as_bytes().to_vec();
+
+    let result = client.send_request(ClientJob::PlayerEnterRequest, &mut name_bytes);
+    if result.is_err() {
+        println!("Error.")
+    }
+
     loop {
+        if client.is_in_error_state() {
+            //println!("Server is in error state...");
+            //thread::sleep(Duration::new(1, 0));
+        }
+
         thread::sleep(pause_time_duration);
         match stuff.trim().as_ref() {
             "q" => {
                 println!("graceful fall...");
                 break;
             }
-            "a" => client.send_request(JobType::DATA_PUSH_ACTION, &mut vec![3, 4, 5, 6]),
-            "s" => client.send_request(JobType::PING, &mut vec![1, 4, 2, 6]),
+            "a" => {
+                let result = client.send_request(ClientJob::DataPushRequest, &mut vec![3, 4, 5, 6]);
+            }
+            "s" => {
+                let result = client.send_request(ClientJob::PlayerEnterRequest, &mut name_bytes);
+            }
+
             _ => {}
         }
     }
