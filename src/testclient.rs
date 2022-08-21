53b5907475def5f06ead14ddf4d4e8d4af028536
L8352 <L8352@student.jamk.fi>
2022-08-21 18:00:56 +0300

    Add stuff to make testclient.rs work with new code.

--- src/testclient.rs
+++ src/testclient.rs
@@ -1,7 +1,14 @@
 use crate::client;
 use crate::client::client::Client;
+use crate::client::datahandlers::playercreatedresponse::PlayerCreatedServerError;
+use crate::client::datahandlers::structs::player::PlayerCreatedResponseData;
+use crate::client::datahandlers::structs::player::PlayerData;
 use crate::client::RequestEvents;
-use crate::requests::jobtype::{ClientJob, ServerJob};
+use crate::protocol::builders::RawDataBuilder;
+use crate::protocol::datahelpers;
+use crate::protocol::datastructure::StructuredData;
+use crate::protocol::Protocol;
+use crate::requests::jobtype::ClientJob;
 use std::io::stdin;
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -46,20 +53,54 @@ impl RequestEvents for Eventful {
         self.count += 1;
     }
 
-    fn on_pong(&mut self, time: Duration) {
+    fn on_pong(&mut self, _time: Duration) {
         todo!()
     }
     fn on_data_push_received(&mut self, player: u8, raw_data: Vec<u8>) {
-        println!("Received from player {}", player);
+        let data_string = String::from_utf8_lossy(&raw_data);
+        println!("Received from player {}: {}", player, data_string);
     }
     fn on_data_request(&mut self, _: std::vec::Vec<u8>) {
         todo!()
     }
-    fn on_player_enter(&mut self, player: u8, raw_data: std::vec::Vec<u8>) {
-        println!("Entered player number {}", player);
+    fn on_player_enter_push(&mut self, player: PlayerData) {
+        println!(
+            "Entered player {} with player number {}. Player addr {}",
+            player.player_name,
+            player.player_number,
+            match player.addr {
+                None => "Just shit...".to_string(),
+                _ => player.addr.unwrap().to_string(),
+            }
+        );
     }
-    fn on_player_created(&mut self, player: u8, raw_data: std::vec::Vec<u8>) {
-        println!("Created player number {}", player);
+    fn on_player_created(
+        &mut self,
+        player_created_response_data_result: Result<
+            PlayerCreatedResponseData,
+            PlayerCreatedServerError,
+        >,
+    ) {
+        match player_created_response_data_result {
+            Err(e) => {
+                print!("{}", e)
+            }
+            Ok(player_created_response_data) => {
+                println!(
+                    "Created player number {}. Your name is: {}\n",
+                    player_created_response_data.player.player_number,
+                    player_created_response_data.player.player_name
+                );
+                for other_player in player_created_response_data.others_players {
+                    println!(
+                        "Other player number {}. Name: {}. Ip: {} \n",
+                        other_player.player_number,
+                        other_player.player_name,
+                        other_player.addr.unwrap().to_string()
+                    );
+                }
+            }
+        }
     }
 
     fn on_player_leave(&mut self, _: std::vec::Vec<u8>) {
@@ -82,52 +123,98 @@ pub fn testclient() {
     println!();
     println!("UDP Rusting client...");
 
-    let server_job = ServerJob::DataResponse;
-    let client_job = ClientJob::DataPushRequest;
+    let protocols = Protocol::new();
+    let player_created_response_protocol = protocols
+        .get_protocol("PlayerCreatedResponse")
+        .expect("No proto");
+    protocols.print_protocol_structures("PlayerEnterPush");
 
-    let byte = crate::requests::jobtype::get_job_single_byte(&(server_job, client_job));
-    println!("{:#b}", byte);
+    let structure = protocols
+        .get_structures_ref("PlayerCreatedResponse")
+        .expect("Not found");
 
-    let server = crate::protocol::datastructure::get_u8_from_bit_slice(byte, 0, 4);
-    let client = crate::protocol::datastructure::get_u8_from_bit_slice(byte, 4, 4);
+    let mut raw_data_builder = RawDataBuilder::new(false)
+        .add_vec_data("Status", structure, vec![30])
+        .expect("Data builder error")
+        .add_vec_data("PlayerNumber", structure, vec![30])
+        .expect("Data builder error")
+        .add_string_data("PlayerName", structure, "Moikka moi".to_string())
+        .expect("Data builder error");
 
-    println!("{},{}", server, client);
+    let array_structure = protocols
+        .get_array_structure_as_ref("PlayerCreatedResponse", "OtherPlayers")
+        .expect("Array structure getter failed");
 
-    /*
-    let first: u8 = 0b10101110;
-    let second: u8 = 0b11110010;
-    let third: u8 = 0b10001111;
+    for i in 1..11 {
+        println!("{}", i);
+        let name_string = match i {
+            1 => "moi".to_string(),
+            2 => "kakonen".to_string(),
+            3 => "kolkki".to_string(),
+            4 => "neli".to_string(),
+            5 => "viiTonen tipiip".to_string(),
+            6 => "kyrpönen".to_string(),
+            7 => "seperi".to_string(),
+            8 => "akrppi".to_string(),
+            9 => "yrjökii".to_string(),
+            10 => "pöö".to_string(),
+            __ => "moi".to_string(),
+        };
 
-    let bytes = vec![first, second, third];
+        let raw_data = RawDataBuilder::new(true)
+            .add_vec_data("PlayerNumber", array_structure, vec![i * 10])
+            .expect("Array builder failed on player number")
+            .add_string_data("PlayerName", array_structure, name_string)
+            .expect("Array builder failed on IP")
+            .add_vec_data("PlayerIP", array_structure, vec![i, i, i, i])
+            .expect("Array builder failed on IP")
+            .add_u16_data("PlayerPort", array_structure, 10000)
+            .expect("Array builder failed on IP")
+            .get_raw_data();
 
-    let mut data_s = DataStructure::new(bytes);
+        raw_data_builder = raw_data_builder
+            .add_array_data("OtherPlayers", structure, raw_data)
+            .expect("Array add failed");
+    }
 
-    data_s.add_structure(
-        "test1".to_string(),
-        4,
-        2,
-        DataType::NUMBERDATA,
-        "Testing 1, count 2 bits".to_string(),
+    let mut structured_data = StructuredData::new(
+        player_created_response_protocol,
+        raw_data_builder.get_raw_data(),
     );
 
-    data_s.add_structure(
-        "test2".to_string(),
-        6,
-        2,
-        DataType::NUMBERDATA,
-        "Testing 2, count 2 bits".to_string(),
-    );
+    structured_data.print_protocol_structures();
 
     println!(
-        "{}",
-        data_s.get_u8_from_bit_slice("test1".to_string()).unwrap()
-    );
-    println!(
-        "{}",
-        data_s.get_u8_from_bit_slice("test2".to_string()).unwrap()
+        "'{}'",
+        structured_data.get_string_data("PlayerName").expect("Shit")
     );
 
-    */
+    println!("{}", structured_data.raw_count());
+
+    let mut indexing = 0;
+    for array_item in structured_data
+        .get_iterable_array("OtherPlayers")
+        .expect("shit")
+    {
+        indexing += 1;
+        print!(
+            "{} -->>> {} ",
+            indexing,
+            array_item.get_string_data("PlayerName").expect("shit")
+        );
+
+        for letter in array_item
+            .get_string_data("PlayerName")
+            .expect("Shit")
+            .chars()
+        {
+            print!("{}-", letter);
+        }
+        println!(
+            "{}",
+            array_item.get_vec_data("PlayerNumber").expect("Shit")[0]
+        );
+    }
 
     println!("port: ");
     let mut local_ip: String = "127.0.0.1:".to_owned();
@@ -163,9 +250,13 @@ pub fn testclient() {
     let mut name = String::new();
     stdin().read_line(&mut name).expect("Shit");
     let name_owned = name.trim().to_owned();
-    let mut name_bytes = name_owned.as_bytes().to_vec();
 
-    let result = client.send_request(ClientJob::PlayerEnterRequest, &mut name_bytes);
+    let raw_name_request_data = datahelpers::create_player_request(&protocols, name_owned);
+
+    let result = client.send_request(
+        ClientJob::PlayerEnterRequest,
+        &mut raw_name_request_data.unwrap(),
+    );
     if result.is_err() {
         println!("Error.")
     }
@@ -179,16 +270,17 @@ pub fn testclient() {
         thread::sleep(pause_time_duration);
         match stuff.trim().as_ref() {
             "q" => {
-                println!("graceful fall...");
+                println!("graceful shutdown...");
                 break;
             }
             "a" => {
-                let result = client.send_request(ClientJob::DataPushRequest, &mut vec![3, 4, 5, 6]);
-            }
-            "s" => {
-                let result = client.send_request(ClientJob::PlayerEnterRequest, &mut name_bytes);
-            }
+                println!("Data string: ");
+                let mut data = String::new();
+                stdin().read_line(&mut data).expect("Shit");
+                let mut data_owned = data.trim().to_owned().as_bytes().to_vec();
 
+                let _result = client.send_request(ClientJob::DataPushRequest, &mut data_owned);
+            }
             _ => {}
         }
     }
