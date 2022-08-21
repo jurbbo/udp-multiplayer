31dc91de4338629dad99b0cc85ca8f3ba6c83ca3
L8352 <L8352@student.jamk.fi>
2022-08-21 17:41:22 +0300

    Use client data handler fns for PLayerEnterPush.

--- src/client/socketlistener.rs
+++ src/client/socketlistener.rs
@@ -1,8 +1,10 @@
+use crate::client::datahandlers::{playercreatedresponse, playerenterpush};
 use crate::client::Arc;
 use crate::client::Mutex;
 use crate::client::RequestEvents;
 use crate::client::UdpSocket;
-use crate::protocol::datastructure::get_u8_from_bit_slice;
+use crate::protocol::bithelpers::get_u8_from_bit_slice;
+use crate::protocol::Protocol;
 use crate::requests::jobs::Jobs;
 use crate::requests::jobtype::get_job_type;
 use crate::requests::jobtype::JobType;
@@ -14,6 +16,7 @@ use std::time::Duration;
 
 pub struct SocketListener<S: 'static> {
     jobs: Arc<Mutex<Jobs>>,
+    protocols: Arc<Protocol>,
     time_to_die: Arc<AtomicBool>,
     socket: Arc<UdpSocket>,
     events: Arc<Mutex<S>>,
@@ -25,6 +28,7 @@ impl<S: RequestEvents + Send + Sync> SocketListener<S> {
     pub fn new(
         jobs: &Arc<Mutex<Jobs>>,
         socket: &Arc<UdpSocket>,
+        protocols: Arc<Protocol>,
         time_to_die: &Arc<AtomicBool>,
         events: &Arc<Mutex<S>>,
         error_state_current: &Arc<AtomicBool>,
@@ -36,6 +40,7 @@ impl<S: RequestEvents + Send + Sync> SocketListener<S> {
         SocketListener {
             jobs: Arc::clone(jobs),
             socket: Arc::clone(socket),
+            protocols: protocols,
             time_to_die: Arc::clone(time_to_die),
             events: Arc::clone(events),
             error_state_current: Arc::clone(error_state_current),
@@ -81,7 +86,9 @@ impl<S: RequestEvents + Send + Sync> SocketListener<S> {
             }
 
             let (job_index, job_type) = index_and_type_maybe.unwrap();
-            self.create_request_event(job_index, job_type.0, raw_data);
+            // [2..] means that we will remove job_index and job_type from raw data.
+            // Protocol datahandler funtions use only data without job index and type.
+            self.create_request_event(job_index, job_type.0, raw_data[2..].to_vec());
         }
     }
 
@@ -105,6 +112,7 @@ impl<S: RequestEvents + Send + Sync> SocketListener<S> {
     }
 
     fn get_index_and_type(&self, raw_data: &Vec<u8>) -> Option<(u8, JobType)> {
+        // package is too short.
         if raw_data.len() < 2 {
             let mut jobs_changer = self.jobs.lock().unwrap();
             (*jobs_changer).packages_failed += 1;
@@ -118,6 +126,7 @@ impl<S: RequestEvents + Send + Sync> SocketListener<S> {
         );
 
         let job_type = get_job_type(&server_client);
+        // package is garbage, does not hold job information.
         if job_type.is_none() {
             let mut jobs_changer = self.jobs.lock().unwrap();
             (*jobs_changer).packages_failed += 1;
@@ -132,13 +141,13 @@ impl<S: RequestEvents + Send + Sync> SocketListener<S> {
         &self,
         job_index: u8,
         server_response_type: ServerJob,
-        raw_data: Vec<u8>,
+        mut raw_data: Vec<u8>,
     ) where
         S: RequestEvents + Send + Sync,
     {
         let mut job_duration = Duration::new(0, 0);
 
-        // Job handling for operations fired from client
+        // Job handling for operations fired from client.
         match &server_response_type {
             ServerJob::DataPush => { /* no job handling for server push operations */ }
             ServerJob::PlayerEnterPush => { /* no job handling */ }
@@ -200,15 +209,45 @@ impl<S: RequestEvents + Send + Sync> SocketListener<S> {
                     let mut events_changer = self.events.lock().unwrap();
                     (*events_changer).on_error();
                     return;
+                }*/
+
+                let response_data_maybe =
+                    playercreatedresponse::structurize_raw_data(&self.protocols, &raw_data);
+
+                let mut events_changer = self.events.lock().unwrap();
+
+                match response_data_maybe {
+                    Err(_) => {
+                        print!("Socket error while reading player created response!");
+                        return;
+                    }
+
+                    Ok(response_data) => match response_data {
+                        Err(e) => {
+                            (*events_changer).on_player_created(Err(e));
+                        }
+                        Ok(player_created_response_data) => {
+                            (*events_changer).on_player_created(Ok(player_created_response_data));
+                        }
+                    },
                 }
-                if raw_data[2] == 1 {
-                    let player_number = raw_data[3];
+
+                /*
+                if raw_data[0] == 1 {
+                    let player_number = raw_data[1];
                     let mut events_changer = self.events.lock().unwrap();
                     (*events_changer).on_player_created(player_number, raw_data);
                 }
+                */
             }
             ServerJob::PlayerEnterPush => {
-                println!("enter");
+                let player_data = playerenterpush::structurize_raw_data(&self.protocols, &raw_data);
+                let mut events_changer = self.events.lock().unwrap();
+                if player_data.is_err() {
+                    (*events_changer).on_error();
+                } else {
+                    (*events_changer).on_player_enter_push(player_data.unwrap());
+                }
             }
             ServerJob::PlayerLeavePush => {}
             ServerJob::PlayerLeaveResponse => {
