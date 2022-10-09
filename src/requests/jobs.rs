ec7f5a06af95f11d51bf3efe5ad4dfccaf8b9e42
L8352 <L8352@student.jamk.fi>
2022-10-09 22:14:24 +0300

    Add spmc worker to handle jobs.

--- src/requests/jobs.rs
+++ src/requests/jobs.rs
@@ -1,134 +1,158 @@
-use crate::requests::jobtype::{get_job_single_byte, JobType};
+use crate::requests::Job;
 use std::collections::HashMap;
 use std::collections::VecDeque;
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::time::Duration;
 use std::time::Instant;
 
-pub struct Job {
-    pub start_instant: Instant,
-    pub pending: bool,
-    pub raw_data: Vec<u8>,
-    pub job_type: JobType,
-    pub requested_count: i8,
-    job_should_complite: u128,
-}
-
-impl Job {
-    pub fn new(
-        handle: u8,
-        job_type: JobType,
-        raw_data: &mut Vec<u8>,
-        job_should_complite: u128,
-    ) -> Job {
-        // raw_data array will be created
-        // first byte is job handle, second byte is jobtype, rest is user data
-        let mut byte_array = vec![handle, get_job_single_byte(&job_type)];
-        byte_array.append(raw_data);
-
-        Job {
-            start_instant: Instant::now(),
-            job_type: job_type,
-            raw_data: byte_array,
-            pending: true,
-            requested_count: 0,
-            job_should_complite: job_should_complite,
-        }
-    }
-
-    pub fn reset_start_instant(&mut self) {
-        self.start_instant = Instant::now();
-    }
-
-    pub fn finish(&mut self) -> Duration {
-        let now = Instant::now();
-        self.pending = false;
-        now.duration_since(self.start_instant)
-    }
-    pub fn get_raw_data(&self) -> Vec<u8> {
-        self.raw_data.to_vec()
-    }
-
-    pub fn is_pending_request_late(&self, now: Instant) -> bool {
-        // only check for pending requests
-        if !self.pending {
-            return false;
-        }
-
-        let job_duration = now.duration_since(self.start_instant);
-        if job_duration.as_millis() > self.job_should_complite {
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
-
 pub struct Jobs {
-    pub jobs: HashMap<u8, Job>,
-    pub next_index: u8,
-    job_finish_times: VecDeque<f64>,
-    pub job_finish_time_average: f64,
-    pub packages_handled: u128,
-    pub packages_failed: u128,
-    average_set_instant: Instant,
+    pub jobs: Arc<Mutex<HashMap<u8, Job>>>,
+    next_job_handle: Mutex<u8>,
+    job_finish_times: Mutex<VecDeque<f64>>,
+    job_finish_time_average: Mutex<f64>,
+    packages_handled: Mutex<u128>,
+    packages_failed: Mutex<u128>,
+    average_set_instant: Mutex<Instant>,
 }
 
 impl Jobs {
     pub fn new() -> Jobs {
         Jobs {
-            jobs: HashMap::<u8, Job>::new(),
-            next_index: 0,
-            job_finish_times: VecDeque::new(),
-            job_finish_time_average: 500.0,
-            packages_handled: 0,
-            packages_failed: 0,
-            average_set_instant: Instant::now(),
+            jobs: Arc::new(Mutex::new(HashMap::<u8, Job>::new())),
+            next_job_handle: Mutex::new(0),
+            job_finish_times: Mutex::new(VecDeque::new()),
+            job_finish_time_average: Mutex::new(500.0),
+            packages_handled: Mutex::new(0),
+            packages_failed: Mutex::new(0),
+            average_set_instant: Mutex::new(Instant::now()),
         }
     }
 
-    pub fn add_finish_time(&mut self, duration: Duration) {
-        self.job_finish_times
-            .push_back((duration.as_micros() as f64) / 1000.0);
-        if self.job_finish_times.len() > 200 {
-            self.job_finish_times.pop_front();
+    pub fn get_ping(&self) -> f64 {
+        let job_finish_time_average_changer = self.job_finish_time_average.lock().unwrap();
+        *job_finish_time_average_changer
+    }
+
+    pub fn add_packages_failed(&self) {
+        let mut packages_failed_changer = self.packages_failed.lock().unwrap();
+        (*packages_failed_changer) += 1;
+    }
+
+    pub fn get_job_finish_time_average(&self) -> f64 {
+        let job_finish_time_average_changer = self.job_finish_time_average.lock().unwrap();
+        *job_finish_time_average_changer
+    }
+
+    pub fn add_packages_handled(&self) {
+        let mut packages_handled_changer = self.packages_handled.lock().unwrap();
+        (*packages_handled_changer) += 1;
+    }
+
+    pub fn insert_job(&self, handle: u8, job: Job) {
+        let mut jobs_changer = self.jobs.lock().unwrap();
+        (*jobs_changer).insert(handle, job);
+        println!("{}", (*jobs_changer).len());
+    }
+
+    pub fn remove_job(&self, handle: u8) {
+        let job_duration_maybe = self.get_job_duration(handle);
+        match job_duration_maybe {
+            None => {}
+            Some(job_duration) => {
+                self.add_finish_time(job_duration);
+            }
         }
+        let mut jobs_changer = self.jobs.lock().unwrap();
+        (*jobs_changer).remove(&handle);
+    }
+
+    /// fns related to ping counting,
+    /// --> job durations are collected to queue,
+    /// --> average is calculated in every 10 secs
+
+    fn get_job_duration(&self, handle: u8) -> Option<Duration> {
+        let mut jobs_changer = self.jobs.lock().unwrap();
+        let job_maybe = jobs_changer.get_mut(&handle);
+        // Job is not found, something went wrong.
+        if job_maybe.is_none() {
+            self.add_packages_failed();
+            // TO DO ERROR STATE MUST BE RAISED!
+            //let mut events_changer = self.events.lock().unwrap();
+            //(*events_changer).on_error();
+            return None;
+        }
+        let job = job_maybe.unwrap();
+        Some(job.finish())
+    }
+
+    fn push_job_duration_to_queue(&self, duration: Duration) {
+        let mut job_finish_times_changer = self.job_finish_times.lock().unwrap();
+
+        (*job_finish_times_changer).push_back((duration.as_micros() as f64) / 1000.0);
+        if (*job_finish_times_changer).len() > 200 {
+            (*job_finish_times_changer).pop_front();
+        }
+    }
+
+    fn has_10_secs_passed_since_last_average_calucation(&self) -> bool {
+        let average_set_instant_changer = self.average_set_instant.lock().unwrap();
         let now = Instant::now();
-        let last_average_set_secs_ago = (now.duration_since(self.average_set_instant)).as_secs();
+        let last_average_set_secs_ago =
+            (now.duration_since(*average_set_instant_changer)).as_secs();
+
+        last_average_set_secs_ago > 9
+    }
+
+    fn set_instant_as_now(&self) {
+        let mut average_set_instant_changer = self.average_set_instant.lock().unwrap();
+        *average_set_instant_changer = Instant::now();
+    }
+
+    pub fn add_finish_time(&self, duration: Duration) {
+        // Jobs collects job durations to calculate
+        self.push_job_duration_to_queue(duration);
+
         // set average every ten seconds
-        if last_average_set_secs_ago > 9 {
-            self.average_set_instant = Instant::now();
+        if self.has_10_secs_passed_since_last_average_calucation() {
+            self.set_instant_as_now();
             self.set_average_finish_time();
         }
     }
 
-    pub fn get_next_index(&mut self) -> u8 {
-        let next_handle = self.next_index;
-        if self.next_index < 255 {
-            self.next_index += 1;
+    pub fn get_next_job_handle(&self) -> u8 {
+        let mut next_job_handle_changer = self.next_job_handle.lock().unwrap();
+        let next_handle_before_addition = *next_job_handle_changer;
+        if *next_job_handle_changer < 255 {
+            *next_job_handle_changer += 1;
         } else {
-            self.next_index = 0;
+            *next_job_handle_changer = 0;
         }
-        next_handle
+        next_handle_before_addition
     }
 
-    fn set_average_finish_time(&mut self) {
+    fn set_average_finish_time(&self) {
         // if there is less than 5 instances of jobs times, return 500 millis just to be sure.
-        if self.job_finish_times.len() < 5 {
-            self.job_finish_time_average = 500.0;
+        let job_finnish_times_changer = self.job_finish_times.lock().unwrap();
+
+        if (*job_finnish_times_changer).len() < 5 {
+            *(self.job_finish_time_average.lock().unwrap()) = 500.0;
             return;
         }
         let mut calculated: f64 = 0.0;
-        for job_finish_time in self.job_finish_times.iter() {
+        for job_finish_time in (*job_finnish_times_changer).iter() {
             calculated += job_finish_time;
         }
-        let average = calculated / (self.job_finish_times.len() as f64);
+        let average = calculated / ((*job_finnish_times_changer).len() as f64);
         println!(
             "calulated average ping {}, packages handled {}, failed {}",
-            average, self.packages_handled, self.packages_failed
+            average,
+            *(self.packages_handled.lock().unwrap()),
+            *(self.packages_failed.lock().unwrap())
         );
 
         // if under 1, use 10.0, and do not use over 500.0 ms average.
-        self.job_finish_time_average = if average <= 1.0 {
+        *(self.job_finish_time_average.lock().unwrap()) = if average <= 1.0 {
             10.0
         } else {
             if average < 500.0 {
---
+++ src/requests/jobworkers.rs
@@ -0,0 +1,122 @@
+use crate::requests::jobs::Jobs;
+use crate::requests::Job;
+use crate::requests::JobAction;
+use std::collections::HashMap;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::Sender;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
+use std::thread::JoinHandle;
+use std::time::Duration;
+use std::time::Instant;
+
+/*
+ * Init job handler
+ *
+ * Job handler checks if there is problematic jobs
+ *
+ * Static function that takes jobs.jobs aka Arc<Mutex<HashMap<u8, Job>>> as first parameter.
+ */
+pub fn run_job_handler(
+    jobs: Arc<Jobs>,
+    socket_send_channel_tx: Sender<(Vec<u8>, Job)>,
+    time_to_die: Arc<AtomicBool>,
+) {
+    loop {
+        thread::sleep(Duration::from_millis(10));
+
+        // break if closing...
+        if time_to_die.load(Ordering::SeqCst) {
+            break;
+        }
+
+        let mut jobs_changer = jobs.jobs.lock().unwrap();
+        let now = Instant::now();
+        let mut failed_job_indexes = Vec::<u8>::new();
+        for (job_handle, job) in &mut *jobs_changer {
+            if job.is_pending_request_late(now) {
+                if job.requested_count < 10 {
+                    // resend failed message
+                    println!("PACKED FAILED. RESEND NUMBER {}!", job.requested_count);
+                    job.reset_start_instant();
+                    job.requested_count += 1;
+                    //let result = socket.send(&job.raw_data);
+                    let result = socket_send_channel_tx.send((job.raw_data.clone(), job.clone()));
+                    if result.is_err() {
+                        panic!("channel error state");
+                    }
+                    // if connection fails, remove job.
+                    if result.is_err() {
+                        job.pending = false;
+                        failed_job_indexes.push(*job_handle);
+                    }
+                // Too many retryes, let's cancel the job.
+                } else {
+                    job.pending = false;
+                    failed_job_indexes.push(*job_handle);
+                }
+            }
+        }
+        // remove failed jobs from index
+        for failed_job_index in failed_job_indexes {
+            println!("index remover {}", failed_job_index);
+            (*jobs_changer).remove(&failed_job_index);
+        }
+    }
+}
+
+/*
+ * Init job channel consumer
+ *
+ * Static function that takes Arc<Jobs> as first parameter.
+ */
+pub fn run_job_channel_consumer(
+    jobs: Arc<Jobs>,
+    job_channel_rx: Receiver<(JobAction, u8, Option<Job>)>,
+    time_to_die: Arc<AtomicBool>,
+) {
+    loop {
+        // break loop if server is closing...
+        if time_to_die.load(Ordering::Relaxed) {
+            break;
+        }
+
+        let result = job_channel_rx.recv();
+        match result {
+            Err(e) => {
+                println!("Job handler consumer failed: {}", e);
+            }
+            Ok(data) => {
+                // tuple of job_action, job_index, and optional job
+                let (job_action, job_handle, job_maybe) = data;
+
+                match job_action {
+                    JobAction::ADD => {
+                        match job_maybe {
+                            None => {}
+                            Some(job) => {
+                                println!("Job created");
+                                // job handle is created with next_job_handle fn,
+                                // when job instance is created
+                                jobs.insert_job(job_handle, job);
+                            }
+                        }
+                    }
+                    JobAction::REMOVE => {
+                        println!("job removed");
+                        jobs.remove_job(job_handle);
+                    }
+                    JobAction::INCFAILED => {
+                        jobs.add_packages_failed();
+                    }
+                    JobAction::INCHANDLED => {
+                        jobs.add_packages_handled();
+                    }
+                }
+            }
+        }
+    }
+}
